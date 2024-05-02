bb410000f98df33af4f8c52485ccd36ac703b5eb
HighwayChile <slickdick101@gmail.com>
2024-05-02 11:18:40 -0700

    yay

---
+++ auto_sorting_list.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+// // for test: use the node assignment like this:
+// int auto_sort_list() {
+//     list my_listl;
+
+//     my_list.display();
+
+//     return 0;
+// }
+// // something like that will instantiate a first node in a linked list.
